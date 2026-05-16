c7bfef04bf1f8121fab7070a0b39f6b7b6fce3d7
Taciano <taciolb@gmail.com>
2026-05-16 18:52:33 -0300

    create login-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from "@angular/router";
 import { RegisterComponent } from './pages/register/register.component';
+import { Login } from "./pages/login/login";
+
 
 // ajuste o caminho conforme sua pasta
 export const routes: Routes = [
-    { path: 'register', component: RegisterComponent}
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: Login}
 ];
