5dd6f698fa7b0f50832e6e0bb0592f8988188083
Taciano <taciolb@gmail.com>
2026-05-16 22:31:21 -0300

    create home and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { RegisterComponent } from './pages/register/register.component';
 import { Login } from "./pages/login/login";
+import { HomeComponent } from "./pages/home/home.component";
 
 
 // ajuste o caminho conforme sua pasta
 export const routes: Routes = [
+    { path: '', component: HomeComponent},
     { path: 'register', component: RegisterComponent},
     { path: 'login', component: Login}
 ];
